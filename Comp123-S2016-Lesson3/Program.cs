48b21c7b8e90df6ba35be23f453f610789444ea4
sbalac17 <sbalac17@my.centennialcollege.ca>
2016-05-20 11:54:35 -0400

    Adding test

---
+++ Comp123-S2016-Lesson3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Tom Tsiliopolos
+ * Date: May 17, 2016
+ * Date Modified: May 17, 2016
+ * Description: Advanced Methods Demo for Lesson 2
+ * Version: 0.0.5 - added readUntilEnd - not completed
+ */
+
+namespace COMP123_S2016_Lesson2
+{
+    /**
+     * Main driver class for Lesson 2
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+        /**
+         * Main method for our drive class
+         * 
+         * @constructor Main
+         * @param {string[]} args
+         */
+        static void Main(string[] args)
+        {
+            int x = 50;
+            int y = 40;
+            int result = 0;
+            int numberOfEntries;
+
+            result = addXandY(x, y);
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            addYtoX(ref x, ref y);
+
+            Console.WriteLine(x);
+
+            Console.WriteLine();
+
+            Console.Write("How many entries do you want to make? ");
+            int.TryParse(Console.ReadLine(), out numberOfEntries);
+            if (numberOfEntries > 0)
+            {
+                readUntilEnd(numberOfEntries);
+            }
+        }
+
+        /**
+         * simple method to add two values
+         * 
+         * @method addXandY
+         * @param {int} firstNumber
+         * @param {int} secondNumber
+         */
+        public static int addXandY(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            result = firstNumber + secondNumber;
+            return result;
+        }
+
+        /**
+         * simple method to demonstrate pass by reference with ref keyword
+         * 
+         * @method addYtoX
+         * @param {ref int} X
+         * @param {ref int} Y
+         */
+        public static int addYtoX(ref int X, ref int Y)
+        {
+            X += Y;
+
+            return X;
+        }
+
+        public static int readUntilEnd(int numberOfEntries)
+        {
+            // variable declaration
+            string[] inputs = new string[numberOfEntries];
+            int inputCounter = 0;
+
+            // execution of code
+            do
+            {
+                Console.WriteLine("Enter a value - ('end' to stop): ");
+                inputs[inputCounter] = "";
+                inputs[inputCounter] = Console.ReadLine();
+                inputCounter++;
+            } while ((inputs[inputCounter - 1] != "end") && (inputCounter < numberOfEntries));
+
+            return inputCounter;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson2;
+
+namespace UnitTestForLesson2
+{
+    [TestClass]
+    public class UnitTestForLesson2
+    {
+        [TestMethod]
+        public void addXandYTestMethod()
+        {
+            // Arrange
+            int x = 40;
+            int y = 50;
+            int expectedResult = 90;
+
+            // Act 
+            int result = Program.addXandY(x, y);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void addYtoXTestMethod()
+        {
+            // Arrange
+            int x = 40;
+            int y = 50;
+            int expectedResult = 90;
+
+            // Act 
+            int result = Program.addYtoX(ref x, ref y);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void readUntilEndTestMethod()
+        {
+            // Arrange
+            int numberOfEntries = 5;
+            int expectedCount = 5;
+            int actualCount = 0;
+
+            // Act 
+            actualCount = Program.readUntilEnd(numberOfEntries);
+
+            // Assert
+            Assert.AreEqual(expectedCount, actualCount);
+
+        }
+    }
+}
